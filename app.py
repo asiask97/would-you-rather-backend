0b432ac5a27ef4ef641a2a633d06ca85bf2c5fd2
Asia <joannaskoc@gmail.com>
2022-07-23 18:59:05 +0100

    POST and GET methods fully working

--- app.py
+++ app.py
@@ -1,28 +1,27 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 from flask_sqlalchemy import SQLAlchemy
 from flask_marshmallow import Marshmallow
+from flask_cors import CORS
+
 
 app = Flask(__name__)
-
-
-
+CORS(app)
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///qanda.db'
+
 # Initialize the database 
 db = SQLAlchemy(app)
 ma = Marshmallow(app)
 # Create Model
 class Questions(db.Model):
     id = db.Column(db.Integer, primary_key = True)
-    question = db.Column(db.String(700), nullable=False)
     optionOne = db.Column(db.String(700), nullable=False)
     optionTwo = db.Column(db.String(700), nullable=False)
-    def __repr__(self):
-        return '<Questions %r%r%r>' % (self.question, self.optionOne, self.optionTwo)
+
 class Answers(db.Model):
     q_id = db.Column(db.Integer, db.ForeignKey('questions.id'), primary_key = True)    
     optionOne = db.Column(db.Integer)
     optionTwo = db.Column(db.Integer)
-
 
 
 class QuestionsShema(ma.SQLAlchemyAutoSchema):
@@ -40,16 +39,42 @@ def index():
         data = Questions.query.all()
         q_schema = QuestionsShema()
         output = q_schema.dump(data, many=True)
-        return jsonify({'questions': output})
+        return jsonify({'qustions': output})
     
     if request.method == 'POST':
+        data = request.json
+        add = Answers.query.filter_by(q_id =data['question']).first()
+        if data['option'] == 'one':
+            if add.optionOne == 0:
+                add.optionOne = 1
+            else:
+                add.optionOne += 1
+
+        if data['option'] == 'two':
+            if add.optionTwo == 0:
+                add.optionTwo = 1
+            else:
+                add.optionTwo += 1
 
 
+        db.session.commit()
+       
+        a_schema = AnswersShema()
+        output = a_schema.dump(add)
+        return jsonify({'awnsers': output})
+    
+
+
+@app.route('/results', methods = ['GET'])
+def anwsers():
+   
+    if request.method == 'GET':
+        data = Answers.query.all()
+        a_schema = AnswersShema()
+        output = a_schema.dump(data, many=True)
+        return jsonify({'anwsers': output})
+    
 
 
 if __name__ == "__main__":
-    app.run(debug = True)
-    db.create_all()
-    db.session.add_all(users)
-    db.session.commit()
-    app.run()
+    app.run(debug=True)
